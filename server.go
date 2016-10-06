f30de8974b78437b57aa1bfbc1f0bddc5f66f706
CJTozer <christopherjtozer@gmail.com>
2016-10-06 22:00:57 +0100

    Picking up missing packages - part 1

--- server.go
+++ server.go
@@ -14,20 +14,15 @@ func main() {
     if port == "" {
         log.Fatal("$PORT must be set")
     }
-
-    // Main page (not on "/" as Static can't be overridden for the data API)
+x
     router := gin.Default()
-    router.Static("/main", "./dist")
-    router.GET("/", func(c *gin.Context) {
-        c.Redirect(http.StatusSeeOther, "/main")
-    })
 
     // Data API (TODO)
     api := router.Group("/data")
     {
         api.GET("/test", func(c *gin.Context) {
             c.JSON(http.StatusOK, gin.H{
-                "message": "pong",
+                "message": "OK!",
             })
         })
     }
