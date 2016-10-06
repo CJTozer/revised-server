32d51e15574c335b4ac87dfcbfa434987244fa5c
Chris Tozer <chris.tozer@metaswitch.com>
2016-10-06 22:32:35 +0100

    Update server with dummy data

--- server.go
+++ server.go
@@ -8,6 +8,11 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+type Book struct {
+    Title string
+    Author string
+}
+
 func main() {
     port := os.Getenv("PORT")
 
@@ -18,11 +23,14 @@ func main() {
     router := gin.Default()
 
     // Data API (TODO)
-    api := router.Group("/data")
+    api := router.Group("/v1/data")
     {
-        api.GET("/test", func(c *gin.Context) {
-            c.JSON(http.StatusOK, gin.H{
-                "message": "OK!",
+        api.GET("/books", func(c *gin.Context) {
+            c.JSON(http.StatusOK, []Book{
+                {"Gardens of the Moon", "Steven Erikson"},
+                {"Longitude", "Dava Sobel"},
+                {"Peter the Great", "Robert K. Massie"},
+                {"The Two Koreas", "Don Oberdorfer"},
             })
         })
     }
