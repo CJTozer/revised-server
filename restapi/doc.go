26dc0f966d475b2d600efacb9ac8a111ff4f7d0e
Chris Tozer <chris.tozer@metaswitch.com>
2016-10-08 10:18:48 +0100

    Initial swagger integration

---
+++ restapi/doc.go
@@ -0,0 +1,22 @@
+/*Package restapi revised API
+
+Data API for revised-web.herokuapp.com
+
+
+    Schemes:
+      https
+    Host: revised-server.herokuapp.com
+    BasePath: /v1
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
