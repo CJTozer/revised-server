ea14b282be302990a1224c2c4fa2e4821db444ac
Chris Tozer <chris.tozer@metaswitch.com>
2016-10-08 15:34:49 +0100

    Add resource list to books

--- backend/books.go
+++ backend/books.go
@@ -10,16 +10,19 @@ func DummyBooksList() []*models.Book {
             Author: "Dava Sobel",
             BookID: 1,
             Title:  "Longitude",
+            Resources: []int64 {1, 3},
         },
         &models.Book{
             Author: "Robert K. Massie",
             BookID: 2,
             Title:  "Peter The Great",
+            Resources: []int64 {2, 4},
         },
         &models.Book{
             Author: "Don Oberdorfer",
             BookID: 3,
             Title:  "The Two Koreas",
+            Resources: []int64 {1, 5},
         },
     }
 }
