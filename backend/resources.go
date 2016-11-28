cb730de05b47afdf3b5e543361b4053c88c4a59c
Chris Tozer <chris.tozer@metaswitch.com>
2016-11-28 18:54:49 +0000

    enum for resource types

--- backend/resources.go
+++ backend/resources.go
@@ -8,52 +8,52 @@ func DummyResourcesList() []*models.Resource {
 return []*models.Resource{
         &models.Resource{
             ID: 1,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource A",
         },
         &models.Resource{
             ID: 2,
             ResourceType: "Link",
-            Text: "Resource B",
+            Text: "https://en.wikipedia.org/wiki/Hyperlink",
         },
         &models.Resource{
             ID: 3,
-            ResourceType: "Link",
-            Text: "Resource C",
+            ResourceType: "Image",
+            Text: "https://upload.wikimedia.org/wikipedia/commons/a/a8/TEIDE.JPG",
         },
         &models.Resource{
             ID: 4,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource D",
         },
         &models.Resource{
             ID: 5,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource E",
         },
         &models.Resource{
             ID: 6,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource F",
         },
         &models.Resource{
             ID: 7,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource G",
         },
         &models.Resource{
             ID: 8,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource H",
         },
         &models.Resource{
             ID: 9,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource I",
         },
         &models.Resource{
             ID: 10,
-            ResourceType: "Link",
+            ResourceType: "Text",
             Text: "Resource J",
         },
     }
--- models/resource.go
+++ models/resource.go
@@ -4,9 +4,13 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"encoding/json"
+
 	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
 
 	"github.com/go-openapi/errors"
+	"github.com/go-openapi/validate"
 )
 
 /*Resource resource
@@ -32,8 +36,46 @@ type Resource struct {
 func (m *Resource) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateResourceType(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+var resourceTypeResourceTypePropEnum []interface{}
+
+// prop value enum
+func (m *Resource) validateResourceTypeEnum(path, location string, value string) error {
+	if resourceTypeResourceTypePropEnum == nil {
+		var res []string
+		if err := json.Unmarshal([]byte(`["Text","Link","Image"]`), &res); err != nil {
+			return err
+		}
+		for _, v := range res {
+			resourceTypeResourceTypePropEnum = append(resourceTypeResourceTypePropEnum, v)
+		}
+	}
+	if err := validate.Enum(path, location, value, resourceTypeResourceTypePropEnum); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Resource) validateResourceType(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.ResourceType) { // not required
+		return nil
+	}
+
+	// value enum
+	if err := m.validateResourceTypeEnum("resource_type", "body", m.ResourceType); err != nil {
+		return err
+	}
+
 	return nil
 }
